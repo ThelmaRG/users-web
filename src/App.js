45c1a2dcb384cdbead4d2cb332fe5418e8df5720
ThelmaRG <thelma_janitzizi@hotmail.com>
2021-02-12 14:07:38 -0600

    UPDATE: Se mueve ubicación de archivos default

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useEffect, useState } from 'react';
+import ComponenteDetallesUsuario from './componentes/ComponenteDetallesUsuario';
+import ComponenteEncabezado from './componentes/ComponenteEncabezado';
+import ComponenteTarjetaUsuario from './componentes/ComponenteTarjetaUsuario';
+import './estilos/Generales.css';
+import { recuperarUsuarios } from './servicios/ServicioUsuarios';
 
 function App() {
+
+  const [arregloUsuarios, setArregloUsuarios] = useState([]);
+  const [muestraDetallesUsuario, setMuestraDetallesUsuario] = useState(false);
+  const [usuarioSeleccionado, setUsuarioSeleccionado] = useState({});
+
+
+
+  useEffect(() => {
+
+    mostrarUsuarios();
+
+  }, []);
+
+  async function mostrarUsuarios() {
+    await recuperarUsuarios().then(respuesta => {
+      setArregloUsuarios(respuesta);
+    }).catch(error => {
+      console.log("ERROR AL RECUPERAR USUARIOS: " + error);
+
+    });
+  }
+
+  function mostrarDetallesUsuario(usuario) {
+    setUsuarioSeleccionado(usuario);
+    setMuestraDetallesUsuario(true);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="Contenedor-Principal">
+      <header>
+        <ComponenteEncabezado />
       </header>
-    </div>
+      <body>
+        <div className="Contenedor-Horizontal">
+          <div className="Contenedor-Vertical">
+            {
+              arregloUsuarios.map((item, index) => (
+                <ComponenteTarjetaUsuario
+                  key={index}
+                  nombre={item.first_name}
+                  apellido={item.last_name}
+                  img={item.avatar}
+                  accionTarjeta={() => mostrarDetallesUsuario(item)}
+                />
+              ))
+            }
+          </div>
+          <div className="Contenedor-Vertical">
+            {muestraDetallesUsuario ?
+              < ComponenteDetallesUsuario
+                nombre={usuarioSeleccionado.first_name}
+                apellido={usuarioSeleccionado.last_name}
+                img={usuarioSeleccionado.avatar}
+                email={usuarioSeleccionado.email}
+                
+              /> : null
+            }
+          </div>
+
+        </div>
+      </body>
+    </div >
   );
 }
 
